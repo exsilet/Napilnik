d6511cbb4704485a3ce8b315836fdd99ef484bad
exsilet <exsilet@mail.ru>
2021-08-21 17:58:09 +0500

    001_encapsulation
    Refactoring encapsulation

---
+++ Task1/Task1/Bot.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1
+{
+    internal class Bot
+    {
+        private readonly Weapon _weapon;
+        private ITarget _target;
+
+        public Bot(Weapon weapon)
+        {
+            _weapon = weapon;
+        }
+
+        public void OnSeePlayer(ITarget target)
+        {
+            _target = target;
+            _weapon.Fire(_target);
+        }
+    }
+}
---
+++ Task1/Task1/Weapon.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1
+{
+    internal class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public Weapon(int damage, int bullets)
+        {
+            _damage = damage;
+            _bullets = bullets;
+        }
+
+        public void Fire(ITarget target)
+        {
+            if (_bullets > 0 && !target.IsAlive)
+            {
+                target.TakeDamage(_damage);
+                _bullets--;
+            }
+        }
+    }
+}
