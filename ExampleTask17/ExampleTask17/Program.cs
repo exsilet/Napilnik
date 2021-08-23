186ce2cc085a9a13e448f4a34fc8bbcc1d2157f1
exsilet <exsilet@mail.ru>
2021-08-23 16:24:53 +0500

    ExampleTask17_Refactoring

---
+++ ExampleTask17/ExampleTask17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExampleTask17
+{
+    class Program
+    {
+        private static int _chance;
+        private static int _hourlyRate;
+
+        public static void CreatingNewObjectTheMap()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void Luck()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int GetPaymentLabor(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
+}
