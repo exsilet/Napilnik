1038208e0337220c0440a6cec284d0552dfb63af
exsilet <exsilet@mail.ru>
2021-08-23 15:36:11 +0500

    ExampleTask11_Refactoring

---
+++ ExampleTask11/ExampleTask11/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExampleTask11
+{
+    class Program
+    {
+        public int SizeArmy = 10;
+        private int _numberCoins = 10;
+        private string _personName = "Vladislav";
+    }
+}
