33cdca404c30870d64d639ea24bdec49df060de8
exsilet <exsilet@mail.ru>
2021-08-23 16:43:25 +0500

    medium8-1-4_Refactoring

---
+++ medium8-1-4/medium8-1-4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace medium8_1_4
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
+
+        public void Attack()
+        {
+            //attack
+        }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Movement
+    {
+        public float MovementSpeed { get; private set; }
+        public float MovementDirectionX { get; private set; }
+        public float MovementDirectionY { get; private set; }
+    }
+
+    class Weapon
+    {
+        public int WeaponDamage { get; private set; }
+        public float WeaponCooldown { get; private set; }
+    }
+}
