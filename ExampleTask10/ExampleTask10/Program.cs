0179599767603219a30daf25631ef4851c729a21
exsilet <exsilet@mail.ru>
2021-08-23 14:48:57 +0500

    ExampleTask10_Refactoring

---
+++ ExampleTask10/ExampleTask10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExampleTask10
+{
+    class Weapon
+    {
+        private const byte _hasNotBullets = 0;
+        private const byte _gunShot = 1;
+        
+        private int _bullets;
+
+        public bool CanShoot() => _bullets > _hasNotBullets;
+
+        public void Shoot() => _bullets -= _gunShot;
+    }
+}
