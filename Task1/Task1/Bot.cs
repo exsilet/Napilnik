b063ed2700ae89bae24fc223963276f18fa3103e
exsilet <exsilet@mail.ru>
2021-08-24 16:02:19 +0500

    01_encapsulation_refactoring

---
+++ Task1/Task1/Bot.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1
+{
+    internal class Bot
+    {
+        private readonly Weapon _weapon;
+        private ITarget _target;
+
+        public Bot(Weapon weapon)
+        {
+            _weapon = weapon;
+        }
+
+        public void OnSeePlayer()
+        {
+            _weapon.Fire(_target);
+        }
+    }
+}
---
+++ Task1/Task1/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public Weapon(int damage, int bullets)
+        {
+            _damage = damage;
+            _bullets = bullets;
+        }
+
+        public void Fire(ITarget target)
+        {
+            if (_bullets > 0)
+            {
+                target.TakeDamage(_damage);
+                _bullets--;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("нет задачи на то когда кончатся патроны");
+            }
+        }
+    }
+}
