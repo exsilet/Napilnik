db85a19af90a562ff5a8ffb1b469d825da39b93d
exsilet <exsilet@mail.ru>
2021-08-24 16:35:35 +0500

    changed_ExampleTask11
    changing _numberCoins to _coins

--- ExampleTask11/ExampleTask11/Program.cs
+++ ExampleTask11/ExampleTask11/Program.cs
@@ -5,7 +5,7 @@ namespace ExampleTask11
     class Program
     {
         public int SizeArmy = 10;
-        private int _numberCoins = 10;
+        private int _coins = 10;
         private string _personName = "Vladislav";
     }
 }
