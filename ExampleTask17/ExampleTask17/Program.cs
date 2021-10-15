ad35d8f5b0b885371501eecf81a98da5a615e561
exsilet <exsilet@mail.ru>
2021-10-15 15:04:31 +0500

    rename function

--- ExampleTask17/ExampleTask17/Program.cs
+++ ExampleTask17/ExampleTask17/Program.cs
@@ -7,17 +7,17 @@ namespace ExampleTask17
         private static int _chance;
         private static int _hourlyRate;
 
-        public static void CreatingNewObjectTheMap()
+        public static void CreatingNewObject()
         {
             //Создание объекта на карте
         }
 
-        public static void Luck()
+        public static void GenerateChance()
         {
             _chance = Random.Range(0, 100);
         }
 
-        public static int GetPaymentLabor(int hoursWorked)
+        public static int GetSalary(int hoursWorked)
         {
             return _hourlyRate * hoursWorked;
         }
